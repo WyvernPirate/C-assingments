b530683a182a4022eb6c93eb31e921f9a16caa40
Phemelo Moloi <moloieric80@gmail.com>
2024-09-23 10:57:11 +0200

    question 3 final

--- Q3.c
+++ Q3.c
@@ -3,20 +3,32 @@
 
 int main(){
     //Declarations and initialisation of variables
-    int choice, quantity, num;
-    float price, sum = 0, discount = 0, discounted_price = 0;
+    int choice, q, num, discount = 0;
+    float p, d, sum = 0, discounted_price = 0;
+    
 
     //Getting number of items from user
     printf("Enter number of unique items :");
     scanf("%d", &num);
 
+    int quantity1[num];
+    float price1[num], subtotal[num];
+
     for (int i = 0; i < num; i++){
         printf("Enter the price of item %d: ",i + 1);
-        scanf("%f", &price);
+        scanf("%f", &p);
         printf("Enter the quantity of item: ");
-        scanf("%d", &quantity);
-        sum += price * quantity;
+        scanf("%d", &q);
+        sum += p * q;
+        price1[i] = p;
+        quantity1[i] = q;
     }
+
+    //Calculating subtotal
+    for(int i = 0; i < num; i++){
+        subtotal[i] = price1[i] * quantity1[i];
+    }
+
 
     //Determining the discount of the items
     if (sum > 5000.0){
@@ -28,15 +40,19 @@ int main(){
     }
     
     //Calculation the price of all items
-    discounted_price = sum - (sum * discount / 100);
+    d = sum * discount / 100;
+    discounted_price = sum - (d);
 
     //Outputting the results
-
-
-    printf("\n\nTotal quantity of items: %d\n", num);
-    printf("Total sum: P%.2f\n", sum);
-    printf("Discount: P%.2f%%\n", discount);
-    printf("Discounted price: P%.2f\n", discounted_price);
+    printf("Item\t\tPrice    Quantity\tSubtotal\n");
+    for(int i = 0;i <num ; i++ ){
+        printf("\nItem %d\t\t%.2f    %d\t\t%.2f",i+1, price1[i], quantity1[i],subtotal[i]);
+    }
+    printf("\n_________________________________________________\n");
+    printf("TOTAL\t\t\t\t\t%.2f\n", sum);
+    printf("Discount: %d %%\n\t\t\t\t\t-%.2f", discount, d);
+    printf("\n_________________________________________________\n");
+    printf("GRAND TOTAL: \t\t\t\t%.2f\n", discounted_price);
 
     return 0;
 }
